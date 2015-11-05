cc810ce84f519d2e5eed59e1b3065f8dec6b279b
Rich Trott <rtrott@gmail.com>
2015-11-04 19:37:55 -0800

    bespoke-secondary

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,7 +8,8 @@ var bespoke = require('bespoke'),
   scale = require('bespoke-scale'),
   hash = require('bespoke-hash'),
   progress = require('bespoke-progress'),
-  forms = require('bespoke-forms');
+  forms = require('bespoke-forms'),
+  secondary = require('bespoke-secondary');
 
 // Bespoke.js
 bespoke.from('article', [
@@ -20,7 +21,8 @@ bespoke.from('article', [
   scale(),
   hash(),
   progress(),
-  forms()
+  forms(),
+  secondary()
 ]);
 
 // Prism syntax highlighting
